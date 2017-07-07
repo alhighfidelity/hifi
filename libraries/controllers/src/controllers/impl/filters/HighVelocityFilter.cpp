10b6b746fa75bca282727873438614c8b1eb18b2
Al Bernstein <al@highfidelity.io>
2017-07-06 20:39:29 -0700

    high velocity filter changes

---
+++ libraries/controllers/src/controllers/impl/filters/HighVelocityFilter.cpp
@@ -0,0 +1,314 @@
+//
+//  Created by Al Bernstein 2017/07/06
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "HighVelocityFilter.h"
+#include <SimpleMovingAverage.h>
+
+
+namespace controller {
+
+    HighVelocityFilter::HighVelocityFilter() {
+        
+        buildOutputArrays();
+    }
+
+    Pose HighVelocityFilter::apply(Pose newPose) const {
+
+        Pose ret;
+
+        std::vector<glm::vec3> test;
+
+        vec3 pos = newPose.getTranslation();
+        quat rot = newPose.getRotation();
+
+        // translation processing
+         
+        size_t N = _posOutput.size();
+
+        if ( N > 0) {
+            glm::vec3 d_pos = diff(_posOutput[0], pos);
+            d_pos = abs(d_pos);
+            pThreshold(d_pos,pos);
+        }
+      
+        updatePosOut(pos);
+
+        // rotation processing
+
+        
+
+       
+
+        return ret;
+    }
+
+    glm::vec3 HighVelocityFilter::diff(glm::vec3 v1, glm::vec3 v2) const {
+        
+        glm::vec3 ret;
+
+        ret = v2 - v1;
+        return ret;
+    }
+
+    void HighVelocityFilter::pThreshold(glm::vec3 v,glm::vec3 pos) const {
+        
+        for (int i = 0; i < 3; i++){
+            if (v[i] > _p_thresh) {
+                processPos(i);
+            }
+        }
+    }
+
+    void HighVelocityFilter::qThreshold(const float &data) const {
+       
+        
+
+    }
+
+    glm::quat HighVelocityFilter::deltaQ(glm::quat q) const {
+
+        if (!_rotOutput.empty()) {
+            _q_ref = _rotOutput[0];
+        }
+
+        glm::quat qtmp = q;
+        qtmp = qFlipHemi(qtmp);
+
+        ThreadSafeMovingAverage<glm::quat, 2> mvAvg;
+
+        mvAvg.addSample(qtmp);
+        glm::quat avg = mvAvg.getAverage();
+        glm::quat qI = glm::inverse(avg);
+        glm::quat ret = qtmp * qI;
+        ret = glm::normalize(ret);
+
+        return ret;
+    }
+
+    glm::quat HighVelocityFilter::qFlipHemi(glm::quat q) const {
+
+        glm::quat ret = q;
+
+        if (glm::dot(_q_ref, q) < 0) {
+            ret = -ret;
+        }
+
+        return ret;
+    }
+
+    float HighVelocityFilter::qDot(glm::quat q1, glm::quat q2) const {
+
+        glm::quat qtmp1 = unitVecAngle(q1);
+        glm::quat qtmp2 = unitVecAngle(q2);
+        qtmp1 = qFlip(qtmp1);
+        qtmp2 = qFlip(qtmp2);
+        float th1 = qtmp1.w;
+        float th2 = qtmp2.w;
+        glm::vec3 v1 = { qtmp1.x, qtmp1.y, qtmp1.z };
+        glm::vec3 v2 = { qtmp2.x, qtmp2.y, qtmp2.z };
+        float ret = th1*th2*glm::dot(v1, v2);
+
+        return ret;
+    }
+
+    glm::quat HighVelocityFilter::qFlip(glm::quat q) const {
+
+        glm::vec3 v = { q.x, q.y, q.z };
+        glm::quat qtmp = q;
+
+        float val = maxIndex(v);
+        if (val < 0) {
+            qtmp = -qtmp;
+        }
+
+        return qtmp;
+    }
+
+    float  HighVelocityFilter::maxIndex(glm::vec3 v)  const {
+
+        float max = -1000.0;
+        int indx = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (abs(v[i]) > max) {
+                max = abs(v[i]);
+                indx = i;
+            }
+        }
+
+        return v[indx];
+    }
+
+    glm::quat HighVelocityFilter::unitVecAngle(glm::quat q) const {
+
+        glm::quat ret = { 0.0f, 0.0f, 0.0f, 0.0f };
+        float psi = 2.0*acos(q.w);
+        float s = sin(psi / 2.0);
+
+        if (s == 0) {
+            if (abs(std::fmod(psi, M_PI)) < 1e-6){
+                ret.x = 0.0f;
+                ret.y = 0.0f;
+                ret.z = 1.0f;
+                ret.w = 0.0f;
+                return ret;
+            }
+            else {
+                ret.x = 0.0f;
+                ret.y = 0.0f;
+                ret.z = -1.0f;
+                ret.w = 0.0;
+
+                return ret;
+            }
+        }
+
+        glm::vec3 uc;
+
+        uc.x = q.x / s;
+        uc.y = q.y / s;
+        uc.z = q.z / s;
+       
+        ret.x = uc.x;
+        ret.y = uc.y;
+        ret.z = uc.z;
+        ret.w = psi;
+
+        return ret;
+    }
+
+
+    void HighVelocityFilter::processPos(const int &k ) const {
+        
+        ThreadSafeMovingAverage<float, 2> mvAvg;
+        std::vector<float> pos = getPos(k, _posOutput);
+        std::vector <float> vPos = createPosStep(pos);
+        std::vector<float> avg;
+
+        for (int i = 0; i < vPos.size(); i++) {
+            mvAvg.addSample(vPos[i]);
+            avg.push_back(mvAvg.getAverage());
+        }
+        setPosOut(k, avg);
+    }
+
+    std::vector<float> HighVelocityFilter::createPosStep(std::vector<float> data) const {
+         
+        std::vector<float> ret;
+        size_t N = data.size();
+
+        if (N == 0) {
+            return ret;
+        }
+       
+        ret.push_back(data[0]);
+
+        for (int i = 1; i < N; i++) {
+            ret.push_back(data[N-1]);
+        }
+
+        return ret;
+
+    }
+
+    std::vector<float>  HighVelocityFilter::getPos(const int &k, std::vector<glm::vec3> v) const {
+
+        std::vector<float> ret;
+
+        for (int i = 0; i < v.size(); i++) {
+            ret.push_back(v[i][k]);
+        }
+
+        return ret;
+    }
+
+    void  HighVelocityFilter::setPosOut(const int &k, std::vector<float> v) const {
+
+
+        for (int i = 0; i < v.size(); i++) {
+            glm::vec3 vtmp;
+            _posOutput[i][k];
+        }
+
+    }
+
+    void HighVelocityFilter::buildOutputArrays() const {
+        
+        _posOutput.clear();
+        _rotOutput.clear();
+        glm::vec3 vtmp = { 0.0f, 0.0f, 0.0f };
+        glm::quat qtmp = { 0.0f, 0.0f, 0.0f, 0.0f };
+       
+        for (int i = 0; i < _N; i++) {
+            _posOutput.push_back(vtmp);
+            _rotOutput.push_back(qtmp);
+        }
+
+    }
+
+    glm::vec3 HighVelocityFilter::updatePosOut(glm::vec3 v) const {
+
+        glm::vec3 ret;
+       
+
+        if (_posOutput.empty()){
+            ret = v;
+            _posOutput.push_back(v);
+            return ret;
+        }
+
+        size_t N = _posOutput.size();
+
+        if (N  < _N) {
+            ret = _posOutput[0];
+            _posOutput.push_back(v);
+            return ret;
+        }
+
+        // N should be equal to _N
+
+        ret = _posOutput[0];
+        std::vector<glm::vec3>::iterator it = _posOutput.begin();
+        _posOutput.erase(it);
+        _posOutput.push_back(v);
+
+       return ret;
+    }
+
+    glm::quat HighVelocityFilter::updateRotOut(glm::quat q) const {
+        
+        glm::quat ret;
+        std::vector<glm::quat>::iterator it;
+
+
+        ret = _rotOutput[_N - 1];
+        _posOutput.pop_back();
+
+        it = _rotOutput.begin();
+        _rotOutput.insert(it, q);
+
+        return ret;
+    }
+
+    bool HighVelocityFilter::parseParameters(const QJsonValue& parameters) {
+
+        if (parameters.isObject()) {
+            auto obj = parameters.toObject();
+            if (obj.contains(JSON_P_THRESHOLD) && obj.contains(JSON_Q_THRESHOLD) && obj.contains(JSON_SIZE) && 
+                obj.contains(JSON_P_WEIGHT) && obj.contains(JSON_Q_WEIGHT)) {
+                _p_thresh = obj[JSON_P_THRESHOLD].toDouble();
+                _q_thresh = obj[JSON_Q_THRESHOLD].toDouble();
+                _N = obj[JSON_SIZE].toInt();
+                //const_cast<int&>(_p_weight) = obj[JSON_P_WEIGHT].toInt();
+                //const_cast<int&>(_q_weight) = obj[JSON_Q_WEIGHT].toInt();
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ libraries/controllers/src/controllers/impl/filters/HighVelocityFilter.h
@@ -0,0 +1,69 @@
+//
+//  Created by Al Bernstein 2017/07/06
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_Controllers_Filters_High_Velocity_h
+#define hifi_Controllers_Filters_High_Velocity_h
+
+#define _USE_MATH_DEFINES
+
+#include "../Filter.h"
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonArray>
+#include <vector>
+
+static const QString JSON_P_THRESHOLD = QStringLiteral("p_threshold");
+static const QString JSON_Q_THRESHOLD = QStringLiteral("q_threshold");
+static const QString JSON_SIZE = QStringLiteral("size");
+static const QString JSON_P_WEIGHT = QStringLiteral("p_weight");
+static const QString JSON_Q_WEIGHT = QStringLiteral("q_weight");
+
+
+namespace controller {
+
+    class HighVelocityFilter : public Filter {
+    REGISTER_FILTER_CLASS(HighVelocityFilter);
+           
+        public:
+            HighVelocityFilter();
+            
+            virtual Pose apply(Pose newPose) const;
+            virtual bool parseParameters(const QJsonValue& parameters) override;
+
+            private:
+           
+                glm::vec3 diff(glm::vec3 v1, glm::vec3 v2) const;
+                void pThreshold(glm::vec3 d_v,glm::vec3 v) const;
+                void qThreshold(const float &data) const;
+                void processPos(const int &k) const;
+                glm::quat  deltaQ(glm::quat qvec) const;
+                float qDot(glm::quat q1, glm::quat q2) const;
+                glm::quat unitVecAngle(glm::quat q) const;
+                glm::quat qFlip(glm::quat q) const;
+                float maxIndex(glm::vec3 v) const;
+                glm::quat qFlipHemi(glm::quat q) const;
+                std::vector<float> createPosStep(std::vector<float> data) const;
+                std::vector<float>  getPos(const int &i, std::vector<glm::vec3> v) const;
+                void setPosOut(const int &k, std::vector<float> v) const;
+                void buildOutputArrays() const;
+                glm::vec3 updatePosOut(glm::vec3 v) const;
+                glm::quat updateRotOut(glm::quat q) const;
+            
+                mutable std::vector<glm::vec3> _posOutput;
+                mutable std::vector<glm::quat> _rotOutput;
+                float _p_thresh{ 0.5f };
+                int _N;
+                float _q_thresh { 0.5f };
+                const int _p_weight{ 2 };
+                const int _q_weight{ 2 };
+                mutable glm::quat _q_ref;
+    };
+
+}
+
+
+#endif
